eebbaae3be8f1778f344bc0b904640874a1fe3d0
Ming <mail2liuming@gmail.com>
2016-06-10 04:44:54 +0000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,21 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar")
+             
+User.create!(name:  "Example User2",
+             email: "example2@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+             
+User.create!(name:  "Example User3",
+             email: "example3@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar")
+             
+users = User.order(:created_at).take(3)
+
+5.times do
+    users.each do |user|
+        user.feeds.create!(flight_no:2,start_place:"hongkong",start_time:"2016-6-10",end_place:"auckland",end_time:"2016-6-29",feed_type: Feed::TYPE_PROVIDER,available:"5")
+    end
+end
